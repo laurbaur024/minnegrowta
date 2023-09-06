3d799bd28feba81be60140a093f579e0518ad0b7
Aidan-Windebank <ajwindybanks@gmail.com>
2023-09-06 12:10:56 -0500

    added api routes and controllers for models

---
+++ server/controllers/flower.controller.js
@@ -0,0 +1,37 @@
+const { Flower } = require('../models');
+const Model = Flower
+
+
+
+// get all flowers
+async function find(criteria = {}){
+  try {
+    const payload = await Model.find(criteria)
+    return payload
+  } catch(err){
+    if(process.env.NODE_ENV === "development") console.log(err)
+    throw new Error(err)
+  }
+}
+
+// get flowers by id
+async function findById(id){
+  try {
+    const payload = await Model.findById(id)
+    return payload
+  } catch(err){
+    if(process.env.NODE_ENV === "development") console.log(err)
+    throw new Error(err)
+  }
+}
+
+
+module.exports = {
+  find,
+  findOne,
+  findById,
+  create,
+  update,
+  updateById,
+  remove
+}
---
+++ server/controllers/planner.controller.js
@@ -0,0 +1,24 @@
+const { User, Plants } = require('../models');
+const Model = { User, Plants }
+
+
+// get all my garden plants
+
+
+
+// get all my journal posts
+
+
+
+
+
+
+module.exports = {
+  find,
+  findOne,
+  findById,
+  create,
+  update,
+  updateById,
+  remove
+}
