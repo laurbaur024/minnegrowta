69a247b8831ef9e492afa99f0b9f7096002b5e19
B Laureen <beffylaureen@me.com>
2023-09-07 20:03:37 -0500

    beth dashboard commit

---
+++ client/src/components/TimelineContainer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Timeline from "./Timeline";
+
+
+const TimelineContainer = () => {
+  return(
+    <div>
+      {Timeline}
+    </div>
+  )
+}
+
+export default TimelineContainer;
