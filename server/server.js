fbe457f10ca4aafc109763e4e99f5015dff3126d
dmtweedy <damon.tweedy@yahoo.com>
2023-09-08 14:45:11 -0500

    stashing changes to grab changes from main

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-require('dotenv').config();
 const express = require('express');
 const cookieParser = require('cookie-parser')
 const path = require('path');
@@ -22,6 +21,10 @@ if (process.env.NODE_ENV === 'production') {
   })
 }
 
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve( __dirname, "../", 'client/build/index.html'))
+})
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+  app.listen(PORT, () => console.log(`Now listening on localhost:${PORT}`));
 });
