2b43d294d308e048c8a8f241032f96ed50ee25e6
J-Stiehl <salsajackie@yahoo.com>
2023-09-10 11:05:19 -0500

    updates

--- client/src/pages/MyFavorites.js
+++ client/src/pages/MyFavorites.js
@@ -38,120 +38,118 @@ export default function MyFavorites(props) {
   }
 
   return (
-
     <div className="fav-content">
-    <>
-      <Grid
-        h="500"
-        templateRows="repeat(2, 1fr)"
-        templateColumns="repeat(5, 1fr)"
-        gap={4}
-      >
-        <GridItem colSpan={1} bg="#85AE5A" m="4" textAlign="center">
-          <Card bg="#85AE5A" size="lg">
-            <CardHeader>
-              <Heading size="md">My Favorite Plants</Heading>
-            </CardHeader>
-            <CardBody>
-              <Text>
-                <Link color="#09302F" href="#">
-                  Roma Tomato
-                </Link>
-              </Text>
-              <Text>
-                <Link color="#09302F" href="#">
-                  Plant 2
-                </Link>
-              </Text>
-              <Text>
-                <Link color="#09302F" href="#">
-                  Plant 3
-                </Link>
-              </Text>
-              <Text>
-                <Link color="#09302F" href="#">
-                  Plant 4
-                </Link>
-              </Text>
-              <Text>
-                <Link color="#09302F" href="#">
-                  Plant 5
-                </Link>
-              </Text>
-              {/* <Button colorScheme="orange">Delete</Button> */}
-            </CardBody>
-          </Card>
-        </GridItem>
+      <>
+        <Grid
+          h="500"
+          templateRows="repeat(2, 1fr)"
+          templateColumns="repeat(5, 1fr)"
+          gap={4}
+        >
+          <GridItem colSpan={1} bg="#85AE5A" m="4" textAlign="center">
+            <Card bg="#85AE5A" size="lg">
+              <CardHeader>
+                <Heading size="md">My Favorite Plants</Heading>
+              </CardHeader>
+              <CardBody>
+                <Text>
+                  <Link color="#09302F" href="#">
+                    Roma Tomato
+                  </Link>
+                </Text>
+                <Text>
+                  <Link color="#09302F" href="#">
+                    Plant 2
+                  </Link>
+                </Text>
+                <Text>
+                  <Link color="#09302F" href="#">
+                    Plant 3
+                  </Link>
+                </Text>
+                <Text>
+                  <Link color="#09302F" href="#">
+                    Plant 4
+                  </Link>
+                </Text>
+                <Text>
+                  <Link color="#09302F" href="#">
+                    Plant 5
+                  </Link>
+                </Text>
+                {/* <Button colorScheme="orange">Delete</Button> */}
+              </CardBody>
+            </Card>
+          </GridItem>
 
-        <GridItem rowSpan={5} colSpan={4} bg="#85AE5A" m="4">
-          <Card bg="#85AE5A">
-            <CardHeader>
-              <Heading size="lg">Roma Tomato</Heading>
-            </CardHeader>
-            <CardBody>
-              <Stack divider={<StackDivider />} spacing="1">
-                <Box
-                  boxSize="400px"
-                  border="2px"
-                  borderColor="black"
-                  borderRadius="20px"
-                  height="268px"
-                >
-                  <img
-                    style={img}
-                    src="https://hgshydro.com:5001/blog/hgs.19105c12-3b2c-4035-9b68-934641c3f27b_default.jpg"
-                    alt=" of Searched Plant"
-                  />
-                </Box>
-                <Box>
-                  <UnorderedList>
-                    <ListItem>
-                      <span style={bold}>Type: </span>Vegetable
-                    </ListItem>
-                    <ListItem>
-                      <span style={bold}>Sow Space: </span>36
-                    </ListItem>
-                    <ListItem>
-                      <span style={bold}>Climbing: </span>no
-                    </ListItem>
-                    <ListItem>
-                      <span style={bold}>Sun: </span>full
-                    </ListItem>
-                    <ListItem>
-                      <span style={bold}>Grow Zone: </span>Zone 3 or higher
-                    </ListItem>
-                    <ListItem>
-                      <span style={bold}>Annual: </span>Yes
-                    </ListItem>
-                    <ListItem>
-                      <span style={bold}>Maturity: </span>70-80 days
-                    </ListItem>
-                  </UnorderedList>
-                </Box>
-              </Stack>
-              <div>
-                <Button style={button} colorScheme="orange">
-                  Remove from Favorites
-                </Button>
-                <Button style={button} colorScheme="orange">
-                  Add to Garden
-                </Button>
-              </div>
-            </CardBody>
-          </Card>
-        </GridItem>
+          <GridItem rowSpan={5} colSpan={4} bg="#85AE5A" m="4">
+            <Card bg="#85AE5A">
+              <CardHeader>
+                <Heading size="lg">Roma Tomato</Heading>
+              </CardHeader>
+              <CardBody>
+                <Stack divider={<StackDivider />} spacing="1">
+                  <Box
+                    boxSize="400px"
+                    border="2px"
+                    borderColor="black"
+                    borderRadius="20px"
+                    height="268px"
+                  >
+                    <img
+                      style={img}
+                      src="https://hgshydro.com:5001/blog/hgs.19105c12-3b2c-4035-9b68-934641c3f27b_default.jpg"
+                      alt=" of Searched Plant"
+                    />
+                  </Box>
+                  <Box>
+                    <UnorderedList>
+                      <ListItem>
+                        <span style={bold}>Type: </span>Vegetable
+                      </ListItem>
+                      <ListItem>
+                        <span style={bold}>Sow Space: </span>36
+                      </ListItem>
+                      <ListItem>
+                        <span style={bold}>Climbing: </span>no
+                      </ListItem>
+                      <ListItem>
+                        <span style={bold}>Sun: </span>full
+                      </ListItem>
+                      <ListItem>
+                        <span style={bold}>Grow Zone: </span>Zone 3 or higher
+                      </ListItem>
+                      <ListItem>
+                        <span style={bold}>Annual: </span>Yes
+                      </ListItem>
+                      <ListItem>
+                        <span style={bold}>Maturity: </span>70-80 days
+                      </ListItem>
+                    </UnorderedList>
+                  </Box>
+                </Stack>
+                <div>
+                  <Button style={button} colorScheme="orange">
+                    Remove from Favorites
+                  </Button>
+                  <Button style={button} colorScheme="orange">
+                    Add to Garden
+                  </Button>
+                </div>
+              </CardBody>
+            </Card>
+          </GridItem>
 
-        <GridItem colSpan={1} bg="#85AE5A" m="4" textAlign="center">
-          <br />
-          Return to Plant Search
-          <br />
-          {/* <Button onClick={redirect()} colorScheme="orange"> */}
-          <Button onClick={handleClick} colorScheme="orange">
-            Return
-          </Button>
-        </GridItem>
-      </Grid>
-    </>
+          <GridItem colSpan={1} bg="#85AE5A" m="4" textAlign="center">
+            <br />
+            Return to Plant Search
+            <br />
+            <Button onClick={handleClick} colorScheme="orange">
+              Return
+            </Button>
+          </GridItem>
+        </Grid>
+      </>
     </div>
   );
 }
